3aa017197f996acb7fb0d29a658bd02f7294ded1
Clayton <claytonprogrammer@gmail.com>
2026-08-25 08:47:24 -0300

    git inicial com esqueleto

---
+++ src/main/java/com/javanauta/revisaobffagendadortarefas/RevisaoBffAgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.revisaobffagendadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisaoBffAgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisaoBffAgendadorTarefasApplication.class, args);
+	}
+
+}
