84aff2aad46fb5fa0dc6c3ed99a6e9d1e6207313
Clayton <claytonprogrammer@gmail.com>
2026-08-27 16:32:12 -0300

    implementado cron

--- src/main/java/com/javanauta/revisaobffagendadortarefas/RevisaoBffAgendadorTarefasApplication.java
+++ src/main/java/com/javanauta/revisaobffagendadortarefas/RevisaoBffAgendadorTarefasApplication.java
@@ -3,9 +3,11 @@ package com.javanauta.revisaobffagendadortarefas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class RevisaoBffAgendadorTarefasApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/javanauta/revisaobffagendadortarefas/business/CronService.java
@@ -0,0 +1,58 @@
+package com.javanauta.revisaobffagendadortarefas.business;
+
+import com.javanauta.revisaobffagendadortarefas.business.dtos.in.LoginDTORequest;
+import com.javanauta.revisaobffagendadortarefas.business.dtos.out.TarefaDTOResponse;
+import com.javanauta.revisaobffagendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CronService {
+
+    private final TarefaService tarefaService;
+    private final EmailService emailService;
+    private final UsuarioService usuarioService;
+
+    @Value("${usuario.email}")
+    private String email;
+
+    @Value("${usuario.senha}")
+    private String senha;
+
+    @Scheduled(cron = "${cron.horario}")
+    public void buscaTarefaProximaHora() {
+        String token = login(converterParaLoginDTORequest());
+        log.info("iniciada a busca de tarefas");
+        LocalDateTime horaFutura = LocalDateTime.now().plusHours(1);
+        LocalDateTime horaFuturaMaisCinco = LocalDateTime.now().plusHours(1).plusMinutes(5);
+
+        List<TarefaDTOResponse> listaTarefas = tarefaService.buscaTarefasAgendadasPorPeriodo(
+                horaFutura, horaFuturaMaisCinco, token);
+        log.info("Tarefas encontradas " + listaTarefas);
+        listaTarefas.forEach(tarefa -> {
+            emailService.enviaEmail(tarefa);
+            log.info("Email enviado para o usuáro " + tarefa.getEmailUsuario());
+            tarefaService.alteraStatusNotificacao(StatusNotificacaoEnum.PENDENTE, tarefa.getId(), token);
+        });
+        log.info("Finalizada a busca e notificação de tarefas");
+    }
+
+    public String login(LoginDTORequest dto) {
+        return usuarioService.loginUsuario(dto);
+    }
+
+    public LoginDTORequest converterParaLoginDTORequest() {
+        return LoginDTORequest.builder()
+                .email(email)
+                .senha(senha)
+                .build();
+    }
+}
